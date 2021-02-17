a45278008d8527f760c3e2f26be21395df13277b
Rifah A Khan <rifahakhan@gmail.com>
2021-02-17 12:44:13 +0000

    Odd or Even

--- Problem_4.c
+++ Problem_4.c
@@ -5,12 +5,18 @@
 
 int main()
 {
-  	int n;
-
+  	int n,i,j;
+    printf("Enter the number of rows");
   	scanf("%d", &n);
+    printf("\n\n\n");
 
   	//Write your code here
-  	
+  	for(i=0;i<n;i++)
+    {
+      for(j=0;j<=i;j++)
+      printf("*");
+      printf("\n");
+    }
 
   	return 0;
 }
